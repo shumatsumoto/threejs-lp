ed125b19a00f085fe7ebef7c786a5e8053af0abc
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-06-19 12:23:32 +0900

    シーン、カメラ、レンダラー追加

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,40 @@ import { setupCounter } from "./counter.ts";
 import * as THREE from "three";
 console.log(THREE);
 
+// キャンバス取得
+const canvas = document.querySelector(".webgl");
+
+// シーン
+const scene = new THREE.Scene();
+
+// サイズ
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// カメラ
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+
+camera.position.z = 6;
+scene.add(camera);
+
+// レンダラー
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.render(scene, camera);
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
